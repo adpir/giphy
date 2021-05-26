83edb6a0b897fdbb5f1d750cc8d80107fc694d49
Alice Piar <adpir30@gmail.com>
2021-05-26 02:05:30 -0500

    create new folder

---
+++ src/components/Helpers/getGifs.js
@@ -0,0 +1,18 @@
+
+export const getGifs = async (category) => {
+
+    const apiKey = '0B8NGW8tHOa0hMTHk4pzP4PdSlTG40gS';
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=${apiKey}`
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+    const giphy = data.map(img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images.downsized_medium.url
+        }
+    })
+    return giphy;
+
+}
+

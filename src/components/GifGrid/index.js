4e8af45c6b3f6efb9daccc3d218fb29792398f72
Alice Piar <adpir30@gmail.com>
2021-05-26 02:04:54 -0500

    update gridfile

--- src/components/GifGrid/index.js
+++ src/components/GifGrid/index.js
@@ -1,43 +1,30 @@
 import React, { useState, useEffect } from 'react'
 import { GifGridItems } from '../GifGridItems';
+import { getGifs } from '../Helpers/getGifs';
 
 
 export const GifGrid = ({ category }) => {
     const [images, setImages] = useState([]);
 
     useEffect(() => {
-        getGifs();
-    }, [])
-
-    const getGifs = async () => {
-
-        const apiKey = '0B8NGW8tHOa0hMTHk4pzP4PdSlTG40gS';
-        const url = `https://api.giphy.com/v1/gifs/search?q=Friends&limit=10&api_key=${apiKey}`
-        const resp = await fetch(url);
-        const { data } = await resp.json();
-        const giphy = data.map(img => {
-            return {
-                id: img.id,
-                title: img.title,
-                url: img.images.downsized_medium.url
-            }
-        })
-        console.log(giphy);
-        setImages(giphy);
-    }
+        getGifs(category)
+            .then(imgs => setImages(imgs))
+    }, [category])
 
 
     return (
         <>
             <h3> {category} </h3>
-            {
-                images.map(img => (
-                    <GifGridItems
-                        key={img.id}
-                        {...img}
-                    />
-                ))
-            }
+            <div className="card-grid">
+                {
+                    images.map(img => (
+                        <GifGridItems
+                            key={img.id}
+                            {...img}
+                        />
+                    ))
+                }
+            </div>
         </>
     )
 }

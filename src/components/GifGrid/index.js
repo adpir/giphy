d612f3669453162776cd5f88b3ab72cdd55508bf
Alice Piar <adpir30@gmail.com>
2021-05-27 23:08:42 -0500

    updated hooks to populate the data

--- src/components/GifGrid/index.js
+++ src/components/GifGrid/index.js
@@ -1,13 +1,15 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react';
+import { useFetchGifts } from '../../hooks/useFetchGifts';
 import { GifGridItems } from '../GifGridItems';
 
 
 export const GifGrid = ({ category }) => {
-    const [images, setImages] = useState([]);
+    const { data: images, loading } = useFetchGifts(category);
 
     return (
         <>
-            <h3> {category} </h3>
+            <h3 className="animate__animated animate__fadeIn"> {category} </h3>
+            {loading && <p className="animate__animated animate__flash">Loading</p>}
             <div className="card-grid">
                 {
                     images.map(img => (
